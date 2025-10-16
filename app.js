d7d4ab060942d92e0ca37f945d2d8e53166c34a1
unknown <Gialinh0167@gmail.com>
2025-10-17 00:00:08 +0700

    Add routes

--- app.js
+++ app.js
@@ -2,11 +2,12 @@
 
 const express = require("express")
 const cors = require("cors");
-
+const contactsRouters = require("./routes/contact.route")
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/contacts", contactsRouters)
 
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to backend1" });
