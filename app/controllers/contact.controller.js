60300a838668323bc99498d40f4f7dde12d2b860
unknown <Gialinh0167@gmail.com>
2025-11-01 07:44:23 +0700

    create new respository

--- app/controllers/contact.controller.js
+++ app/controllers/contact.controller.js
@@ -23,23 +23,19 @@ exports.create = async (req, res, next) => {
 
 exports.findAll = async (req, res, next) => {
   let documents = [];
-
   try {
     const contactService = new ContactService(MongoDB.client);
     const { name } = req.query;
-
     if (name) {
       documents = await contactService.findByName(name);
     } else {
-      documents = await contactService.find({});
-    }
+      documents = await contactService.find({});}
   } catch (error) {
     console.log(error)
     return next(
       new ApiError(500, "An error occurred while retrieving contacts")
     );
   }
-
   return res.send(documents);
 };
 
@@ -50,8 +46,7 @@ exports.findOne = async (req, res, next) => {
     const contactService = new ContactService(MongoDB.client);
     const document = await contactService.findById(req.params.id);
     if (!document) {
-      return next(new ApiError(404, "Contact not found"));
-    }
+      return next(new ApiError(404, "Contact not found"));}
     return res.send(document);
   } catch (error) {
     console.log(error)
@@ -68,17 +63,12 @@ exports.findOne = async (req, res, next) => {
 // Update a contact by the id in the request
 exports.update = async (req, res, next) => {
   if (Object.keys(req.body).length === 0) {
-    return next(new ApiError(400, "Data to update can not be empty"));
-  }
-
+    return next(new ApiError(400, "Data to update can not be empty"));}
   try {
     const contactService = new ContactService(MongoDB.client);
     const document = await contactService.update(req.params.id, req.body);
-
     if (!document) {
-      return next(new ApiError(404, "Contact not found"));
-    }
-
+      return next(new ApiError(404, "Contact not found"));}
     return res.send({ message: "Contact was updated successfully" });
   } catch (error) {
     console.log(error)
@@ -96,11 +86,8 @@ exports.delete = async (req, res, next) => {
   try {
     const contactService = new ContactService(MongoDB.client);
     const document = await contactService.delete(req.params.id);
-
     if (!document) {
-      return next(new ApiError(404, "Contact not found"));
-    }
-
+      return next(new ApiError(404, "Contact not found"));}
     return res.send({ message: "Contact was deleted successfully" });
   } catch (error) {
     console.log("Error in delete:", error);
